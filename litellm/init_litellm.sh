08e612b47ca722926378ff47ab649ecde83a9e1c
eilamshapira <eilamshapira@campus.technion.ac.il>
2025-05-30 19:25:36 +0300

    Add initial configuration for litellm

---
+++ litellm/init_litellm.sh
@@ -0,0 +1,7 @@
+export OPENAI_API_KEY="Enter your OpenAI API key here"
+export XAI_API_KEY="Enter your XAI API key here"
+export GOOGLE_APPLICATION_CREDENTIALS=litellm/google_key.json
+
+export LITELLM_CONFIG_PATH=litellm/model_config.yaml
+
+# litellm --config litellm/model_config.yaml
